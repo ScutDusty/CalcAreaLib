3c41757f77c96adf6d5c8ededf6a3b414e361e0a
ScutDusty <46516288+ScutDusty@users.noreply.github.com>
2020-05-09 22:06:44 +0300

    Add files via upload

---
+++ Query.sql
@@ -0,0 +1,22 @@
+﻿SELECT 
+	Products.Name,
+	Categories.Name
+FROM Products
+	LEFT JOIN ProdCat ON Products.Id = ProdCat.ProductsId
+	LEFT JOIN Categories ON ProdCat.CategoriesId=Categories.Id
+
+/*
+RESULT
+
+1	Horse	Animal
+2	Horse	Picture
+3	Pig	Sculpture
+4	Snake	NULL
+5	Sun	NULL
+6	Flower	NULL
+7	Tree	NULL
+8	Man	NULL
+9	Woman	Picture
+10	Woman	Sculpture
+11	Dog	NULL
+*/
---
+++ SQLTables.sql
@@ -0,0 +1,51 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [Id]   INT  NOT NULL,
+    [Name] TEXT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Products] (
+    [Id]   INT  NOT NULL,
+    [Name] TEXT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[ProdCat] (
+    [ProductsId]   INT NOT NULL,
+    [CategoriesId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([ProductsId] ASC, [CategoriesId] ASC),
+    FOREIGN KEY ([ProductsId]) REFERENCES [dbo].[Products] ([Id]),
+    FOREIGN KEY ([CategoriesId]) REFERENCES [dbo].[Categories] ([Id])
+);
+
+INSERT INTO Products
+VALUES
+(1, 'Horse'),
+(2, 'Pig'),
+(3, 'Snake'),
+(4, 'Sun'),
+(5, 'Flower'),
+(6, 'Tree'),
+(7, 'Man'),
+(8, 'Woman'),
+(9, 'Dog');
+
+INSERT INTO Categories
+VALUES
+(1, 'Animal'),
+(2, 'Toy'),
+(3, 'Picture'),
+(4, 'Sculpture');
+
+INSERT INTO ProdCat
+VALUES
+(1, 1),
+(1, 3),
+(2, 4),
+(8, 3),
+(8, 4);
+
+
+
+
+
